919778c9e8ed1ecb56aeee231e8fa1e4e49daecc
Dan Tarjeft <dtarjeft@gmail.com>
2015-02-19 17:28:17 -0500

    Changed Multiplication of Expressions when one is a number and the other is not (multiplication across parenthesis). Passes more unit tests.

--- MathParser/Expression.cs
+++ MathParser/Expression.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Text;
 using Sprache;
 
 namespace MathParser
@@ -18,11 +20,15 @@ namespace MathParser
         {
             _isNum = isNum;
             _contents = isNegative ? "-" + statement : statement;
-            Console.WriteLine("{1} _isNum: {0}", _isNum, _contents);
         }
 
-        private readonly string _contents;
-        private readonly bool _isNum;
+        private string _contents;
+        private bool _isNum;
+
+        private static List<string> SplitByChar(string a, char[] charToFactorBy)
+        {
+            return new List<string>(a.Split(charToFactorBy, StringSplitOptions.RemoveEmptyEntries));
+        }
 
         private static bool IsANumber(string statement)
         {
@@ -38,7 +44,8 @@ namespace MathParser
         {
             if (this._isNum && expression._isNum)
             {
-                return new Expression((Convert.ToDouble(_contents) - Convert.ToDouble(expression._contents)).ToString(), true);
+                _contents = (Convert.ToDouble(_contents) - Convert.ToDouble(expression._contents)).ToString();
+                return this;
             }
             return new Expression(_contents + "-" + expression._contents, false);
         }
@@ -58,8 +65,70 @@ namespace MathParser
             {
                 return new Expression((Convert.ToDouble(_contents) * Convert.ToDouble(expression._contents)).ToString(), true);
             }
+            else if (this._isNum && !expression._isNum)
+            {
+                var factorThat = SplitByChar(expression._contents, new[] {'+'});
+                var combinedList = factorThat.Select(str => str.Insert(0, _contents + "*")).ToList();
+                var result = new StringBuilder();
+                for (var index = 0; index < combinedList.Count; index++)
+                {
+                    var t = combinedList[index];
+                    var tsubTokens = new List<string>(t.Split(new[] {'*'}, StringSplitOptions.RemoveEmptyEntries));
+                    if (tsubTokens.Count() > 1)
+                    {
+                        tsubTokens.Insert(0, "1");
+                      
+                        for (var i = 1; i < tsubTokens.Count; i++)
+                        {
+                            var token = tsubTokens[i];
+                            if (!IsANumber(token)) continue;
+                            tsubTokens[0] = (Convert.ToDouble(tsubTokens[0])*Convert.ToDouble(token)).ToString();
+                            tsubTokens[i] = string.Empty;
+                        }
+                        var mergedOutput = new StringBuilder();
+                        tsubTokens.RemoveAll(EmptyString);
+                        for (var i = 0; i < tsubTokens.Count; i++)
+                        {
+                            var tsubToken = tsubTokens[i];
+                            mergedOutput.Append(tsubToken);
+                            if (i < tsubTokens.Count - 1)
+                            {
+                                mergedOutput.Append("*");
+                            }
+                        }
+                        
+                        t = mergedOutput.ToString();
+                    }
+                    result.Append(t);
+                    if (index < combinedList.Count - 1)
+                    {
+                        result.Append("+");
+                    }
+                }
+                _contents = result.ToString();
+                _isNum = false;
+                return this;
+            }
+            else
+            {
+                var chArray = new []{'+'};
+                var factorThis = SplitByChar(this._contents, chArray);
+                var factorThat = SplitByChar(expression._contents, chArray);
+                //var result = Recombine(factorThis, factorThat, new[] {'*'});
+            }
             return new Expression(_contents + "*" + expression._contents, false);
         }
+
+        private static bool EmptyString(string obj)
+        {
+            return (obj == string.Empty);
+        }
+
+        //private static string Recombine(List<string> s1, List<string> s2, char[] c)
+        //{
+        //    var result = new List<string>();
+
+        //}
 
         public Expression Divide(Expression expression)
         {
